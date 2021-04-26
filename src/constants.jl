2e61afb489092050fe5453f36b5661cf33efd0b7
Richard Styron <richard.h.styron@gmail.com>
2021-04-26 10:07:29 -0700

    added EARTH_RAD_M to constants.jl

--- src/constants.jl
+++ src/constants.jl
@@ -1,8 +1,10 @@
 module Constants
 
-export EARTH_RAD_KM, EARTH_RAD_MM
+export EARTH_RAD_KM, EARTH_RAD_M, EARTH_RAD_MM
 
 const EARTH_RAD_KM = 6371.
+
+const EARTH_RAD_M = EARTH_RAD_KM * 1e3
 
 const EARTH_RAD_MM = EARTH_RAD_KM * 1e6
 
