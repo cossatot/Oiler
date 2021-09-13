f98522d7e7d1372693aaa1fbe3e0f92a4fb71e89
Richard Styron <richard.h.styron@gmail.com>
2021-09-13 11:22:01 -0700

    updated docs build and gitlab ci

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,6 @@
 using Documenter, Oiler
 
-makedocs(;
-    modules=[Oiler],
-    format=Documenter.HTML(assets=String[]),
-    pages=[
-        "Home" => "index.md",
-        "Examples" => "examples.md",
-        "Module" => map(
-            s -> "docstrings/$(s)",
-            sort(readdir(joinpath(@__DIR__, "src/docstrings")))
-        ),
-    ],
-    # repo="https://gitlab.com/cossatot/Oiler.jl/blob/{commit}{path}#L{line}",
-    sitename="Oiler",
-    authors="Richard Styron",
-    doctest=false,
-)
-
+include("makedocs.jl")
 
 deploydocs(
     repo="github.com/cossatot/Oiler.git",
