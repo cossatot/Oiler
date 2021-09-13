4765bf0751a3ab80d9c03485e0745f7a3d8e0809
Richard Styron <richard.h.styron@gmail.com>
2021-09-13 11:33:54 -0700

    ci fix

---
+++ docs/makedocs.jl
@@ -0,0 +1,19 @@
+using Documenter, Oiler
+
+makedocs(;
+    modules=[Oiler],
+    format=Documenter.HTML(assets=String[]),
+    pages=[
+        "Home" => "index.md",
+        "Examples" => "examples.md",
+        "Module" => map(
+            s -> "docstrings/$(s)",
+            sort(readdir(joinpath(@__DIR__, "src/docstrings")))
+        ),
+    ],
+    # repo="https://gitlab.com/cossatot/Oiler.jl/blob/{commit}{path}#L{line}",
+    sitename="Oiler",
+    authors="Richard Styron",
+    doctest=false,
+)
+
