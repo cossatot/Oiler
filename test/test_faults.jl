8d2b0a7c60a9ffccad787490778df478a15f4917
Richard Styron <richard.h.styron@gmail.com>
2021-12-13 10:20:28 -0800

    unit test updates

--- test/test_faults.jl
+++ test/test_faults.jl
@@ -4,85 +4,73 @@ using Oiler
 
 
 function test_fault_slip_rate_to_ve_vn()
-    sqrt2 = sqrt(2.)
-    test_dict = Dict((1., 0., 0.) => (0., 1.),
-        (1., 0., 45.) => (sqrt2 / 2., sqrt2 / 2.),
-        (1., 0., 90.) => (1., 0.),
-        (1., 0., 135.) => (sqrt2 / 2., -sqrt2 / 2.),
-        
-        (0., 1., 0.) => (-1., 0.),
-        (0., 1., 45.) => (-sqrt2 / 2., sqrt2 / 2),
-        (0., 1., 90.) => (0., 1.),
-        (0., 1., 135.) => (sqrt2 / 2., sqrt2 / 2.),
-
-        (1., 1., 0.) => (-1., 1),
-        (-1., 1., 0.) => (-1, -1),
-        (1., -1., 0.) => (1, 1),
-        (-1., -1., 0.) => (1, -1),
-
-        (1., 1., 45.) => (0., sqrt2),
-        (-1., 1., 45.) => (-sqrt2, 0.),
-        (1., -1., 45.) => (sqrt2, 0.),
-        (-1., -1., 45.) => (0., -sqrt2),
-        (1., 1., 90.) => (1., 1.),
-        (-1., 1., 90.) => (-1., 1.),
-        (1., -1., 90.) => (1., -1.),
-        (-1., -1., 90.) => (-1., -1.),
-        (1., 1., 135.) => (sqrt2, 0.),
-        (-1., 1., 135.) => (0., sqrt2),
-        (1., -1., 135.) => (0., -sqrt2),
-        (-1., -1., 135.) => (-sqrt2, 0.))
+    sqrt2 = sqrt(2.0)
+    test_dict = Dict((1.0, 0.0, 0.0) => (0.0, 1.0),
+        (1.0, 0.0, 45.0) => (sqrt2 / 2.0, sqrt2 / 2.0),
+        (1.0, 0.0, 90.0) => (1.0, 0.0),
+        (1.0, 0.0, 135.0) => (sqrt2 / 2.0, -sqrt2 / 2.0), (0.0, 1.0, 0.0) => (-1.0, 0.0),
+        (0.0, 1.0, 45.0) => (-sqrt2 / 2.0, sqrt2 / 2),
+        (0.0, 1.0, 90.0) => (0.0, 1.0),
+        (0.0, 1.0, 135.0) => (sqrt2 / 2.0, sqrt2 / 2.0), (1.0, 1.0, 0.0) => (-1.0, 1),
+        (-1.0, 1.0, 0.0) => (-1, -1),
+        (1.0, -1.0, 0.0) => (1, 1),
+        (-1.0, -1.0, 0.0) => (1, -1), (1.0, 1.0, 45.0) => (0.0, sqrt2),
+        (-1.0, 1.0, 45.0) => (-sqrt2, 0.0),
+        (1.0, -1.0, 45.0) => (sqrt2, 0.0),
+        (-1.0, -1.0, 45.0) => (0.0, -sqrt2),
+        (1.0, 1.0, 90.0) => (1.0, 1.0),
+        (-1.0, 1.0, 90.0) => (-1.0, 1.0),
+        (1.0, -1.0, 90.0) => (1.0, -1.0),
+        (-1.0, -1.0, 90.0) => (-1.0, -1.0),
+        (1.0, 1.0, 135.0) => (sqrt2, 0.0),
+        (-1.0, 1.0, 135.0) => (0.0, sqrt2),
+        (1.0, -1.0, 135.0) => (0.0, -sqrt2),
+        (-1.0, -1.0, 135.0) => (-sqrt2, 0.0))
 
     for (inputs, outputs) in test_dict
-        @test isapprox(collect(Oiler.fault_slip_rate_to_ve_vn(inputs...)), 
-                       collect(outputs))
+        @test isapprox(collect(Oiler.fault_slip_rate_to_ve_vn(inputs...)),
+            collect(outputs))
     end
 end
 
 
 function test_ve_vn_to_fault_slip_rate()
-    sqrt2 = sqrt(2.)
+    sqrt2 = sqrt(2.0)
     test_dict = Dict(
-        (0., 1., 0.) =>  (1., 0.),
-        (sqrt2 / 2., sqrt2 / 2., 45.) => (1., 0.),
-        (1., 0., 90.) => (1., 0.),
-        (sqrt2 / 2., -sqrt2 / 2., 135.) => (1., 0.),
-
-        (-1., 0., 0.) => (0., 1.),
-        (-sqrt2 / 2., sqrt2 / 2, 45.) => (0., 1.),
-        (0., 1., 90.) => (0., 1.),
-        (0., 1.,   135.) => (sqrt2 / 2., sqrt2 / 2.),
-
-        (1., 1.,   0.)   => (-1., 1),
-        (-1., 1.,  0.)   => (-1, -1),
-        (1., -1.,  0.)   => (1, 1),
-        (-1., -1., 0.)   => (1, -1),
-
-        (0., sqrt2,  45.) => (1., 1.),
-        (-sqrt2, 0., 45.) =>  (-1., 1.),
-        (sqrt2, 0., 45.) => (1., -1.),
-        (0., -sqrt2, 45.) => (-1., -1.),
-        (1., 1., 90.) => (1., 1.),
-        (-1., 1., 90.) => (-1., 1.),
-        (1., -1., 90.) => (1., -1.),
-        (-1., -1., 90.) => (-1., -1.),
-        (sqrt2, 0., 135.) => (1., 1.),
-        (0., sqrt2, 135.) => (-1., 1.),
-        (0., -sqrt2, 135.) => (1., -1.),
-        (-sqrt2, 0., 135.) => (-1., -1.)
+        (0.0, 1.0, 0.0) => (1.0, 0.0),
+        (sqrt2 / 2.0, sqrt2 / 2.0, 45.0) => (1.0, 0.0),
+        (1.0, 0.0, 90.0) => (1.0, 0.0),
+        (sqrt2 / 2.0, -sqrt2 / 2.0, 135.0) => (1.0, 0.0), (-1.0, 0.0, 0.0) => (0.0, 1.0),
+        (-sqrt2 / 2.0, sqrt2 / 2, 45.0) => (0.0, 1.0),
+        (0.0, 1.0, 90.0) => (0.0, 1.0),
+        (0.0, 1.0, 135.0) => (sqrt2 / 2.0, sqrt2 / 2.0), (1.0, 1.0, 0.0) => (-1.0, 1),
+        (-1.0, 1.0, 0.0) => (-1, -1),
+        (1.0, -1.0, 0.0) => (1, 1),
+        (-1.0, -1.0, 0.0) => (1, -1), (0.0, sqrt2, 45.0) => (1.0, 1.0),
+        (-sqrt2, 0.0, 45.0) => (-1.0, 1.0),
+        (sqrt2, 0.0, 45.0) => (1.0, -1.0),
+        (0.0, -sqrt2, 45.0) => (-1.0, -1.0),
+        (1.0, 1.0, 90.0) => (1.0, 1.0),
+        (-1.0, 1.0, 90.0) => (-1.0, 1.0),
+        (1.0, -1.0, 90.0) => (1.0, -1.0),
+        (-1.0, -1.0, 90.0) => (-1.0, -1.0),
+        (sqrt2, 0.0, 135.0) => (1.0, 1.0),
+        (0.0, sqrt2, 135.0) => (-1.0, 1.0),
+        (0.0, -sqrt2, 135.0) => (1.0, -1.0),
+        (-sqrt2, 0.0, 135.0) => (-1.0, -1.0)
     )
 
     for (inputs, outputs) in test_dict
         @test isapprox(collect(Oiler.Faults.ve_vn_to_fault_slip_rate(inputs...)),
-        collect(outputs))
+            collect(outputs))
     end
 end
 
-quien_sabe_coords = [-121.37575 36.93768;
-                     -121.33534 36.89357;
-                     -121.29735 36.85596;
-                     -121.25795 36.81047;
-                     -121.2073 36.75499];
+quien_sabe_coords = [-121.37575 36.93768
+    -121.33534 36.89357
+    -121.29735 36.85596
+    -121.25795 36.81047
+    -121.2073 36.75499];
 
 
 function test_get_midpoint_old()
@@ -96,15 +84,15 @@ end
 
 
 function test_fault_to_vel_1()
-    qs_1 = Oiler.Fault(trace=quien_sabe_coords, dip=89., dip_dir="NE", 
-        name="Quien Sabe NE dip", hw="gab", fw="hol", dextral_rate=35.)
+    qs_1 = Oiler.Fault(trace = quien_sabe_coords, dip = 89.0, dip_dir = "NE",
+        name = "Quien Sabe NE dip", hw = "gab", fw = "hol", dextral_rate = 35.0)
 
     vel = Oiler.Faults.fault_to_vel(qs_1)
 
     @test vel.lon == -121.28985450382699
     @test vel.lat == 36.84731110410578
-    @test vel.ve == -20.908201158796516
-    @test vel.vn == 28.06861457755441
+    @test vel.ve == -20.77427972024876
+    @test vel.vn == 28.167877131670057
     @test vel.vu == 0.0
     @test vel.ee == 0.0
     @test vel.en == 0.0
@@ -117,15 +105,15 @@ end
 
 
 function test_fault_to_vel_2()
-    qs_2 = Oiler.Fault(trace=quien_sabe_coords, dip=89., dip_dir="SW",
-        name="Quien Sabe S dip", hw="hol", fw="gab", dextral_rate=35.)
+    qs_2 = Oiler.Fault(trace = quien_sabe_coords, dip = 89.0, dip_dir = "SW",
+        name = "Quien Sabe S dip", hw = "hol", fw = "gab", dextral_rate = 35.0)
 
     vel = Oiler.Faults.fault_to_vel(qs_2)
 
     @test vel.lon == -121.28985450382699
     @test vel.lat == 36.84731110410578
-    @test vel.ve == 20.718245578662074
-    @test vel.vn == -28.209117322990622
+    @test vel.ve == 20.786879116086872
+    @test vel.vn == -28.158580514883763
     @test vel.vu == 0.0
     @test vel.ee == 0.0
     @test vel.en == 0.0
@@ -138,15 +126,15 @@ end
 
 
 function test_fault_to_vel_3()
-    qs_3 = Oiler.Fault(trace=quien_sabe_coords, dip=90., dip_dir="V", 
-        name="Quien Sabe V dip", hw="hol", fw="gab", dextral_rate=35.)
+    qs_3 = Oiler.Fault(trace = quien_sabe_coords, dip = 90.0, dip_dir = "V",
+        name = "Quien Sabe V dip", hw = "hol", fw = "gab", dextral_rate = 35.0)
 
     vel = Oiler.Faults.fault_to_vel(qs_3)
 
     @test vel.lon == -121.28985450382699
     @test vel.lat == 36.84731110410578
-    @test vel.ve == 20.718245578662074
-    @test vel.vn == -28.209117322990622
+    @test vel.ve == 20.786879116086872
+    @test vel.vn == -28.158580514883763
     @test vel.vu == 0.0
     @test vel.ee == 0.0
     @test vel.en == 0.0
@@ -170,87 +158,86 @@ function test_check_right_hand_rule_flip()
     trace_in = quien_sabe_coords
     dip_dir = "NE"
     trace_out = Oiler.Faults.check_right_hand_rule(trace_in, dip_dir)
-    @test reverse(trace_in, dims=1) == trace_out
+    @test reverse(trace_in, dims = 1) == trace_out
 end
 
 
 function test_get_fault_slip_rate_from_pole_reverse_no_change()
-    fault = Oiler.Fault(trace=[5. 0.; -5. 0.], dip_dir="N", dip=30., hw="N", fw="S")
-    pole = Oiler.PoleCart(x=-4.312474367375016e-10, 
-        y=-1.5696123057604772e-10, z=0., mov="S", fix="N")
+    fault = Oiler.Fault(trace = [5.0 0.0; -5.0 0.0], dip_dir = "N", dip = 30.0, hw = "N", fw = "S")
+    pole = Oiler.PoleCart(x = -4.312474367375016e-10,
+        y = -1.5696123057604772e-10, z = 0.0, mov = "S", fix = "N")
 
     fsr = Oiler.Faults.get_fault_slip_rate_from_pole(fault, pole)
 
-    @test isapprox(fsr[1], 0., atol=1e-10)
-    @test isapprox(fsr[2], -1., atol=1e-10)
+    @test isapprox(fsr[1], 0.0, atol = 1e-10)
+    @test isapprox(fsr[2], -1.0, atol = 1e-10)
 end
 
 
 function test_get_fault_slip_rate_from_pole_reverse_flip_pole()
-    fault = Oiler.Fault(trace=[-5. 0.; 5. 0.], dip_dir="S", dip=30., hw="S", fw="N")
-    pole = Oiler.PoleCart(x=-4.312474367375016e-10, 
-        y=-1.5696123057604772e-10, z=0., mov="S", fix="N")
+    fault = Oiler.Fault(trace = [-5.0 0.0; 5.0 0.0], dip_dir = "S", dip = 30.0, hw = "S", fw = "N")
+    pole = Oiler.PoleCart(x = -4.312474367375016e-10,
+        y = -1.5696123057604772e-10, z = 0.0, mov = "S", fix = "N")
 
     fsr = Oiler.Faults.get_fault_slip_rate_from_pole(fault, pole)
 
-    @test isapprox(fsr[1], 0., atol=1e-10)
-    @test isapprox(fsr[2], -1., atol=1e-10)
+    @test isapprox(fsr[1], 0.0, atol = 1e-10)
+    @test isapprox(fsr[2], -1.0, atol = 1e-10)
 end
 
 
 function test_get_fault_slip_rate_from_pole_mismatch()
-    fault = Oiler.Fault(trace=[-5. 0.; 5. 0.], dip_dir="S", dip=30., hw="N", fw="S")
-    pole = Oiler.PoleCart(x=-4.312474367375016e-10, 
-        y=-1.5696123057604772e-10, z=0., mov="S", fix="A")
+    fault = Oiler.Fault(trace = [-5.0 0.0; 5.0 0.0], dip_dir = "S", dip = 30.0, hw = "N", fw = "S")
+    pole = Oiler.PoleCart(x = -4.312474367375016e-10,
+        y = -1.5696123057604772e-10, z = 0.0, mov = "S", fix = "A")
 
     fsr = Oiler.Faults.get_fault_slip_rate_from_pole(fault, pole)
 
-    @test isapprox(fsr[1], 0., atol=1e-10)
-    @test isapprox(fsr[2], 1., atol=1e-10)
+    @test isapprox(fsr[1], 0.0, atol = 1e-10)
+    @test isapprox(fsr[2], 1.0, atol = 1e-10)
 end
 
 
 
 function test_get_fault_slip_rate_from_pole_strikeslip_no_change()
-    fault = Oiler.Fault(trace=[0. -5.; 0. 5.], dip_dir="E", dip=89., hw="E", fw="W")
-    pole = Oiler.PoleCart(x=-4.312474367375016e-10, 
-        y=-1.5696123057604772e-10, z=0., mov="W", fix="E")
+    fault = Oiler.Fault(trace = [0.0 -5.0; 0.0 5.0], dip_dir = "E", dip = 89.0, hw = "E", fw = "W")
+    pole = Oiler.PoleCart(x = -4.312474367375016e-10,
+        y = -1.5696123057604772e-10, z = 0.0, mov = "W", fix = "E")
 
     fsr = Oiler.Faults.get_fault_slip_rate_from_pole(fault, pole)
 
-    @test isapprox(fsr[1], 1., atol=1e-10)
-    @test isapprox(fsr[2], 0., atol=1e-10)
+    @test isapprox(fsr[1], 1.0, atol = 1e-10)
+    @test isapprox(fsr[2], 0.0, atol = 1e-10)
 end
 
 
 function test_get_fault_slip_rate_from_pole_strikeslip_flip()
-    fault = Oiler.Fault(trace=[0. -5.; 0. 5.], dip_dir="W", dip=89., hw="W", fw="E")
-    pole = Oiler.PoleCart(x=-4.312474367375016e-10, 
-        y=-1.5696123057604772e-10, z=0., mov="W", fix="E")
+    fault = Oiler.Fault(trace = [0.0 -5.0; 0.0 5.0], dip_dir = "W", dip = 89.0, hw = "W", fw = "E")
+    pole = Oiler.PoleCart(x = -4.312474367375016e-10,
+        y = -1.5696123057604772e-10, z = 0.0, mov = "W", fix = "E")
 
     fsr = Oiler.Faults.get_fault_slip_rate_from_pole(fault, pole)
 
-    @test isapprox(fsr[1], 1., atol=1e-10)
-    @test isapprox(fsr[2], 0., atol=1e-10)
+    @test isapprox(fsr[1], 1.0, atol = 1e-10)
+    @test isapprox(fsr[2], 0.0, atol = 1e-10)
 end
 
 
-function test_fault_to_vels()
-end
+function test_fault_to_vels() end
 
 
 function test_fault_oblique_merc()
 
-    ff = Oiler.Fault(trace=[83. 32.; 83.5 32.8], dip=60., dip_dir="SE")
+    ff = Oiler.Fault(trace = [83.0 32.0; 83.5 32.8], dip = 60.0, dip_dir = "SE")
 
-    lons = [100.; 101.; 122.]
-    lats = [20.; 20.; 80.5]
+    lons = [100.0; 101.0; 122.0]
+    lats = [20.0; 20.0; 80.5]
 
     x, y = Oiler.Faults.fault_oblique_merc(ff, lons, lats)
 
     # these values from faultobliquemerc.m by Meade and Loveless
     x_ = [-0.995670130919819; -0.985654810206086; -0.099467883321707]
-    y_ = [-0.342577075025567; -0.356822693071572;  0.271564430987158]
+    y_ = [-0.342577075025567; -0.356822693071572; 0.271564430987158]
 
     @test isapprox(x[1:3], x_ .* Oiler.EARTH_RAD_KM)
     @test isapprox(y[1:3], y_ .* Oiler.EARTH_RAD_KM)
@@ -258,22 +245,20 @@ end
 
 
 function test_strike_rot_matrix_xd()
-    xd_partials = [0. 0. 0.; 0. 1. 0.; 0. 0. 0.]
-    strike = 45.
+    xd_partials = [0.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 0.0]
+    strike = 45.0
 
     srt = Oiler.Faults.build_strike_rot_matrix(strike)
 
-    v = srt * xd_partials * [0.; 1.; 0.]
-    @test isapprox(v, [-0.7071067811865475; 0.7071067811865475; 0.])
+    v = srt * xd_partials * [0.0; 1.0; 0.0]
+    @test isapprox(v, [-0.7071067811865475; 0.7071067811865475; 0.0])
 end
 
 
-function test_build_Pf_vert()
-end
+function test_build_Pf_vert() end
 
 
-function test_build_Pf_dip()
-end
+function test_build_Pf_dip() end
 
 
 
@@ -281,24 +266,24 @@ end
 function test_partials_and_rotation_matrices_1()
     # not sure what is supposed to happen in this function
 
-    fault = Oiler.Fault(trace=[-5. 0.; 5. 0.], dip_dir="S", dip=30.)
-    
-    pp = Oiler.Elastic.calc_locking_effects_per_fault(fault, [0.], [-0.0001])[1]
+    fault = Oiler.Fault(trace = [-5.0 0.0; 5.0 0.0], dip_dir = "S", dip = 30.0)
 
-    pole = [-4.312474367375016e-10; -1.5696123057604772e-10; 0.]
+    pp = Oiler.Elastic.calc_locking_effects_per_fault(fault, [0.0], [-0.0001])[1]
+
+    pole = [-4.312474367375016e-10; -1.5696123057604772e-10; 0.0]
 
     pp * pole
 end
 
 
 function test_build_velocity_projection_matrix_1()
-    Pf = Oiler.build_velocity_projection_matrix(90., 90.)
+    Pf = Oiler.build_velocity_projection_matrix(90.0, 90.0)
 
-    @test isapprox(Pf * [1.; 0.; 0.], [1.; 0.; 0.])
+    @test isapprox(Pf * [1.0; 0.0; 0.0], [1.0; 0.0; 0.0])
 end
 
 
-@testset begin
+@testset "test faults.jl" begin
     test_fault_to_vel_1()
     test_fault_to_vel_2()
     test_fault_to_vel_3()
--- test/test_geom.jl
+++ test/test_geom.jl
@@ -4,44 +4,44 @@ import Oiler
 
 
 function test_gc_dist()
-    lax_lon = -(118. + 24. / 60.);
-    lax_lat = 33. + 57. / 60.;
+    lax_lon = -(118.0 + 24.0 / 60.0)
+    lax_lat = 33.0 + 57.0 / 60.0
 
-    jfk_lon = -(73. + 47. / 60.);
-    jfk_lat = 40. + 38. / 60.;
+    jfk_lon = -(73.0 + 47.0 / 60.0)
+    jfk_lat = 40.0 + 38.0 / 60.0
 
     @test Oiler.gc_distance(lax_lon, lax_lat, jfk_lon, jfk_lat) == 3972.857776250374
 end
 
 
 function test_azimuth_1()
-    lax_lon = -(118. + 24. / 60.);
-    lax_lat = 33. + 57. / 60.;
+    lax_lon = -(118.0 + 24.0 / 60.0)
+    lax_lat = 33.0 + 57.0 / 60.0
 
-    jfk_lon = -(73. + 47. / 60.);
-    jfk_lat = 40. + 38. / 60.;
-    
+    jfk_lon = -(73.0 + 47.0 / 60.0)
+    jfk_lat = 40.0 + 38.0 / 60.0
+
     @test Oiler.azimuth(lax_lon, lax_lat, jfk_lon, jfk_lat) == 65.89216655274531
 end
 
 
 function test_average_azimuth()
-    quien_sabe_coords = [-121.37575 36.93768;
-                         -121.33534 36.89357;
-                         -121.29735 36.85596;
-                         -121.25795 36.81047;
-                         -121.2073 36.75499];
-    
-    @test Oiler.average_azimuth(quien_sabe_coords[:,1], quien_sabe_coords[:,2]) ==
-    143.70450041501906
+    quien_sabe_coords = [-121.37575 36.93768
+        -121.33534 36.89357
+        -121.29735 36.85596
+        -121.25795 36.81047
+        -121.2073 36.75499]
+
+    @test Oiler.average_azimuth(quien_sabe_coords[:, 1], quien_sabe_coords[:, 2]) ==
+          143.56497333710072
 end
 
 
 function test_oblique_merc_2()
     lat1 = 0.00001
     lat2 = -0.00001
-    lon1 = -1.
-    lon2 = 1.
+    lon1 = -1.0
+    lon2 = 1.0
 
     lons = [0.25; 0.35]
     lats = [0.25; 0.25]
@@ -51,31 +51,31 @@ function test_oblique_merc_2()
 end
 
 function test_terminal_coords_from_bearing_dist_1()
-    @test Oiler.Geom.terminal_coords_from_bearing_dist(0., 0., 0., 10.) == 
-    (0.0, 0.08993216059187306)
+    @test Oiler.Geom.terminal_coords_from_bearing_dist(0.0, 0.0, 0.0, 10.0) ==
+          (0.0, 0.08993216059187306)
 end
 
 function test_terminal_coords_from_bearing_dist_2()
-    @test Oiler.Geom.terminal_coords_from_bearing_dist(0., 89.5, 0., 100.) ==
-    (180.0, 89.60067839408168)
+    @test Oiler.Geom.terminal_coords_from_bearing_dist(0.0, 89.5, 0.0, 100.0) ==
+          (180.0, 89.60067839408168)
 end
 
 function test_terminal_coords_from_bearing_dist_3()
-    @test Oiler.Geom.terminal_coords_from_bearing_dist(0., 0., 90., 10.) ==
-    (0.08993216059187306, 0.0)
+    @test Oiler.Geom.terminal_coords_from_bearing_dist(0.0, 0.0, 90.0, 10.0) ==
+          (0.08993216059187306, 0.0)
 end
 
 
 function test_polyline_length()
-    trace = [0. 0.; 0. 0.5; 0. 1.]
+    trace = [0.0 0.0; 0.0 0.5; 0.0 1.0]
     @test Oiler.Geom.polyline_length(trace) == 111.19492664455873
 end
 
 
 function test_point_line_distance_1()
-    point = [1., 1.]
-    line_start = [0., 0.]
-    line_end = [2., 0.]
+    point = [1.0, 1.0]
+    line_start = [0.0, 0.0]
+    line_end = [2.0, 0.0]
     dist = Oiler.Geom.point_line_distance(point, line_start, line_end)
 
     @test dist == 1.0
@@ -83,9 +83,9 @@ end
 
 
 function test_point_line_distance_2()
-    point = [0., 0.]
-    line_start = [0., 0.]
-    line_end = [2., 0.]
+    point = [0.0, 0.0]
+    line_start = [0.0, 0.0]
+    line_end = [2.0, 0.0]
     dist = Oiler.Geom.point_line_distance(point, line_start, line_end)
 
     @test dist == 0.0
@@ -93,85 +93,94 @@ end
 
 
 function test_simplify_polyline_vert()
-    polyline = [0. 0.; 0. 1.; 0. 2.; 0. 3.; 0. 4.]
-    simp_line = [0. 0.; 0. 4.]
-    test_simp_line = Oiler.Geom.simplify_polyline(polyline, 0.)
+    polyline = [0.0 0.0; 0.0 1.0; 0.0 2.0; 0.0 3.0; 0.0 4.0]
+    simp_line = [0.0 0.0; 0.0 4.0]
+    test_simp_line = Oiler.Geom.simplify_polyline(polyline, 0.0)
+
     @test test_simp_line == simp_line
 end
 
 
 function test_simplify_polyline_rosettacode()
-    polyline = [0. 0.; 1. 0.1; 2. -0.1; 3. 5.; 4. 6.; 
-                5. 7.; 6. 8.1; 7. 9.; 8. 9.; 9. 9.]
-    simp_line = [0. 0.; 2. -0.1; 3. 5.; 7. 9.; 9. 9.]
-    test_simp_line = Oiler.Geom.simplify_polyline(polyline, 1.)
+    polyline = [0.0 0.0
+        1.0 0.1
+        2.0 -0.1
+        3.0 5.0
+        4.0 6.0
+        5.0 7.0
+        6.0 8.1
+        7.0 9.0
+        8.0 9.0
+        9.0 9.0]
+    simp_line = [0.0 0.0; 2.0 -0.1; 3.0 5.0; 7.0 9.0; 9.0 9.0]
+    test_simp_line = Oiler.Geom.simplify_polyline(polyline, 1.0)
     @test test_simp_line == simp_line
 end
 
 
 function test_break_polyline_equal_1()
-    tr = [0. 0.; 3. 3.; 9. 9.; 10. 10.]
+    tr = [0.0 0.0; 3.0 3.0; 9.0 9.0; 10.0 10.0]
     @test Oiler.Geom.break_polyline_equal(tr, 1) == [tr]
 end
 
 function test_break_polyline_equal_2()
-    tr = [0. 0.; 3. 3.; 9. 9.; 10. 10.]
-    @test Oiler.Geom.break_polyline_equal(tr, 2) == 
-    [[0.0 0.0; 3.0 3.0; 4.980085962376654 5.000771545105226],
-    [4.980085962376654 5.000771545105226; 9.0 9.0; 10.0 10.0]]
+    tr = [0.0 0.0; 3.0 3.0; 9.0 9.0; 10.0 10.0]
+    @test Oiler.Geom.break_polyline_equal(tr, 2) ==
+          [[0.0 0.0; 3.0 3.0; 4.980085962376654 5.000771545105226],
+        [4.980085962376654 5.000771545105226; 9.0 9.0; 10.0 10.0]]
 
 end
 
 function test_break_polyline_equal_3()
-    tr = [0. 0.; 3. 3.; 9. 9.; 10. 10.]
+    tr = [0.0 0.0; 3.0 3.0; 9.0 9.0; 10.0 10.0]
     @test Oiler.Geom.break_polyline_equal(tr, 4) ==
-    [[0.0 0.0; 2.493526173498305 2.4945825672649673],
-    [2.493526173498305 2.4945825672649673; 3.0 3.0; 4.980085962376654 5.000771545105226],
-    [4.980085962376654 5.000771545105226; 7.481540598820497 7.50169718205662],
-    [7.481540598820497 7.50169718205662; 9.0 9.0; 10.0 10.0]]
+          [[0.0 0.0; 2.493526173498305 2.4945825672649673],
+        [2.493526173498305 2.4945825672649673; 3.0 3.0; 4.980085962376654 5.000771545105226],
+        [4.980085962376654 5.000771545105226; 7.481540598820497 7.50169718205662],
+        [7.481540598820497 7.50169718205662; 9.0 9.0; 10.0 10.0]]
 end
 
 
 function test_check_winding_order_array_2_1()
-    aa = [0. 0.; 1. 0.; 1. 1.; 0. 1.; 0. 0.]
+    aa = [0.0 0.0; 1.0 0.0; 1.0 1.0; 0.0 1.0; 0.0 0.0]
     @test Oiler.Geom.check_winding_order(aa) == 1
 end
 
 function test_check_winding_order_array_arrays_1()
-    bb = [[0. 0.], [1. 0.], [1. 1.], [0. 1.], [0. 0.]]
+    bb = [[0.0 0.0], [1.0 0.0], [1.0 1.0], [0.0 1.0], [0.0 0.0]]
     @test Oiler.Geom.check_winding_order(bb) == 1
 end
 
 
 function test_strike_dip_from_3_pts()
-    pt1 = [0., 0., 0.]
-    pt2 = [0., 1., 0.]
-    pt3 = [1., 0.5, -1.]
+    pt1 = [0.0, 0.0, 0.0]
+    pt2 = [0.0, 1.0, 0.0]
+    pt3 = [1.0, 0.5, -1.0]
 
     strike_1, dip_1 = Oiler.Geom.strike_dip_from_3_pts(pt1, pt2, pt3)
-    @test strike_1 == 0.
-    @test dip_1 == 45.
+    @test strike_1 == 0.0
+    @test dip_1 == 45.0
 
-    pt4 = [0.5, 0.5, 0.]
-    pt5 = [1., 0., -1.]
+    pt4 = [0.5, 0.5, 0.0]
+    pt5 = [1.0, 0.0, -1.0]
 
     strike_2, dip_2 = Oiler.Geom.strike_dip_from_3_pts(pt1, pt4, pt5)
-    @test strike_2 == 45.
-    @test isapprox(dip_2,  54.735610317245346)
+    @test strike_2 == 45.0
+    @test isapprox(dip_2, 54.735610317245346)
 
-    pt6 = [-1., -1., 0.]
-    pt7 = [0., -1., -1.]
+    pt6 = [-1.0, -1.0, 0.0]
+    pt7 = [0.0, -1.0, -1.0]
 
     strike_3, dip_3 = Oiler.Geom.strike_dip_from_3_pts(pt1, pt6, pt7)
-    @test strike_3 == 45.
-    @test isapprox(dip_3,  54.735610317245346)
+    @test strike_3 == 45.0
+    @test isapprox(dip_3, 54.735610317245346)
 
-    pt8 = [0., -1., 1.]
+    pt8 = [0.0, -1.0, 1.0]
     strike_4, dip_4 = Oiler.Geom.strike_dip_from_3_pts(pt1, pt6, pt8)
-    @test strike_4 == 225.
-    @test isapprox(dip_4,  54.735610317245346)
+    @test strike_4 == 225.0
+    @test isapprox(dip_4, 54.735610317245346)
 
-    pt9 = [0., 0.5, -1.]
+    pt9 = [0.0, 0.5, -1.0]
     # fails due to SingularException.  Could add a fix?
     # strike_5, dip_5 = Oiler.Geom.strike_dip_from_3_pts(pt1, pt2, pt9)
     # @test strike_5 == 0.
